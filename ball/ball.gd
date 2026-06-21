96e7a166f01e8176d5b5c3593b94ae5aecf04604
PlumpChamp <dandylion140206@gmail.com>
2026-06-21 13:30:12 +0900

    2

--- ball/ball.gd
+++ ball/ball.gd
@@ -2,9 +2,10 @@ extends Area2D
 
 signal speed_updated(speed: float)
 
-@onready var movement: BallMovement = $BallMovement
+@onready var movement: Movement = $Movement
 
 var velocity: Vector2 = Vector2.ZERO
+
 
 func _process(delta: float) -> void:
 	velocity = movement.update_velocity(
--- ball/movement.gd
+++ ball/movement.gd
@@ -1,9 +1,10 @@
-class_name BallMovement
+class_name Movement
 extends Node
 
-@export_range(5000, 30000, 1000) var acceleration: int = 10000
-@export_range(1000, 10000, 200) var target_speed: int = 5000
-@export_range(1000, 10000, 200) var max_speed: int = 5000
+
+@export_range(5000, 30000, 100) var acceleration: int = 10000
+@export_range(1000, 10000, 100) var target_speed: int = 5000
+@export_range(1000, 20000, 100) var max_speed: int = 10000
 
 var velocity := Vector2.ZERO
 
